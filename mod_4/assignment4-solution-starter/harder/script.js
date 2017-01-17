9080a2d652b35e9cad346537855d27961a0fa7ce
SaideepGona <gona.saideep1@gmail.com>
2017-01-17 12:51:28 -0500

    Assignment 4 more edits

--- mod_4/assignment4-solution-starter/harder/script.js
+++ mod_4/assignment4-solution-starter/harder/script.js
@@ -34,7 +34,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-(function () {
+(function (window) {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -61,11 +61,11 @@ for (name_index = 0; name_index < names.length; name_index++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (name.charAt(0).toLowerCase == 'j') {
-    byeSpeaker.speak(name)
+    window.byeSpeaker.speak(name)
   } else {
-    helloSpeaker.speak(namme)
+    window.helloSpeaker.speak(name)
   }
 }
 
 
-}) ();
+}) (window);
