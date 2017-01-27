a1b11ac74dcda2efbde0f52054b97ed7485e4071
SaideepGona <gona.saideep1@gmail.com>
2017-01-27 17:59:33 -0500

    small fix on assignment5

--- mod_5/assignment5/assignment5-solution-starter/js/script.js
+++ mod_5/assignment5/assignment5-solution-starter/js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
 
-      insertHtml("#maincontent",newhomeHtml)
+      insertHtml("#main-content",newhomeHtml)
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
